de99887163501816811e49ee93b4521ade627a9e
DESKTOP-F433F9N\MANUELA PINEDA <manup102010@hotmail.com>
2023-11-08 15:25:53 -0500

    configurationNode and sequentialo progr.

---
+++ progra-101/app.ts
@@ -0,0 +1,13 @@
+let numberOfLines = 1;
+console.log('Línea #', numberOfLines);
+
+numberOfLines = numberOfLines + 1;
+console.log('Línea #', numberOfLines);
+
+numberOfLines = numberOfLines + 1;
+console.log('Línea #', numberOfLines);
+
+numberOfLines = numberOfLines + 1;
+console.log('Línea #', numberOfLines);
+
+
