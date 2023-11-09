bd2efea71f55cfae5f8d2ce92bf8c61aa7a2b2a1
DESKTOP-F433F9N\MANUELA PINEDA <manup102010@hotmail.com>
2023-11-09 11:08:43 -0500

    Finalización sesion#1

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,13 +1,2 @@
-let numberOfLines = 1;
-console.log('Línea #', numberOfLines);
-
-numberOfLines = numberOfLines + 1;
-console.log('Línea #', numberOfLines);
-
-numberOfLines = numberOfLines + 1;
-console.log('Línea #', numberOfLines);
-
-numberOfLines = numberOfLines + 1;
-console.log('Línea #', numberOfLines);
-
+console.log('Hola Mundo');
 
---
+++ progra-101/sections/section-2.ts
@@ -0,0 +1,15 @@
+let numberOfLines = 1;
+
+export function printLineNumber() {
+
+    console.log('Línea #', numberOfLines);
+    numberOfLines ++;
+}
+
+printLineNumber();
+printLineNumber();
+printLineNumber();
+printLineNumber();
+
+
+
