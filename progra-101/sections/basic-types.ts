176c64808ce377c1e84cb81e3af66dc2490b11de
DESKTOP-F433F9N\MANUELA PINEDA <manup102010@hotmail.com>
2023-11-10 10:05:21 -0500

    basic types

---
+++ progra-101/sections/basic-types.ts
@@ -0,0 +1,22 @@
+//Strings
+export let firstName: string = 'Manuela';
+let lastName: string = 'Pineda'
+
+let fullName: string = firstName + " " + lastName;
+
+console.log(fullName);
+
+//Numbers
+let number1: number = 10;
+let number2: number = 20;
+
+console.log(number1 - number2);
+
+//Booleans
+let isActive: boolean = true;
+let isRunning: boolean = false;
+let hasKids: boolean = true;
+
+console.log('isActive', !isActive);
+console.log('isRunning', isRunning);
+console.log('hasKids', hasKids);
