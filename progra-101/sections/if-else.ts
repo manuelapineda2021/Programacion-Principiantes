da9ce39db252c168d11b1b6d4ab75e4fe3f2b874
DESKTOP-F433F9N\MANUELA PINEDA <manup102010@hotmail.com>
2023-12-04 12:15:08 -0500

    control structure and taks of if-else

---
+++ progra-101/sections/if-else.ts
@@ -0,0 +1,13 @@
+console.log("Inicio de programa");
+
+export let grade:number = 40;
+
+if(grade >= 60){
+    console.log('El alumno aprueba la clase');
+}else if(grade > 50){
+    console.log('Por favor estudie más!');
+}else{
+    console.log('El alumno NO aprueba la clase');
+}
+
+console.log("Fin del programa");
