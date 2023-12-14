3a86da0f8c870f6f4b1ed92763012b9622a00c1d
DESKTOP-F433F9N\MANUELA PINEDA <manup102010@hotmail.com>
2023-12-14 18:02:49 -0500

    task arrays and end  of session

---
+++ progra-101/sections/Section-4/arrays.ts
@@ -0,0 +1,9 @@
+export let people: string[] = ['Manuela', 'Alvaro', 'Hakuna'];
+let flowers: string[] = ['Girasol', 'Rosa', 'Lirio', 'Rosa azul'];
+
+for(let i=0; i<flowers.length; i++){
+
+    console.log('En la posición ' + i + ' esta la flor: ' + flowers[i]);
+}
+
+
---
+++ progra-101/sections/Section-4/task-arrays.ts
@@ -0,0 +1,11 @@
+export let employees:string[] = ['Manuela', 'Alvaro', 'Hakuna'];
+let salaries:number [] = [1500, 2400, 3200]; //Salario en dolares de las personas
+
+
+for(let i=0; i< employees.length; i++){
+
+    console.log(employees[i] + ' tiene un salario de: ' + salaries[i]);
+
+}
+
+
