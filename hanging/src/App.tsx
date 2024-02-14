997d88a3affbff84023ac97ac2a3e15ad12cfbf0
manuelapineda2021 <manup102010@hotmail.com>
2024-02-14 12:26:19 -0500

    images, attemps and word

--- hanging/src/App.tsx
+++ hanging/src/App.tsx
@@ -1,11 +1,47 @@
+import { useState } from 'react';
+import { letters } from './helpers/letters';
+import { HangImage } from './components/HangImage';
 import './App.css'
+
 
 function App() {
 
+  const [word] = useState('COMPUTADORA');
+  const [hiddenword] = useState('_ '.repeat(word.length));
+  const [attempts, setAttempts] = useState(0);
+
+  //Oprimir Letra
+  const checkLetter = (letter: string) => {
+
+    console.log(letter)
+    setAttempts(Math.min(attempts + 1, 9)); //Números de intentos no pasen de 9
+
+  }
+
   return (
-    
+
     <div className='App'>
-      <h1>Hola mundo</h1>
+
+      {/* Imagenes*/}
+      <HangImage imageNumber={attempts} />
+
+
+      {/* Palabra Oculata*/}
+      <h3>{hiddenword}</h3>
+
+      {/* Contador de Intentos*/}
+      <h3>Intentos: {attempts} </h3>
+
+      {/* Botones de Letras*/}
+
+      {
+        letters.map((letter) => (
+          <button
+            onClick={() => checkLetter(letter)}
+            key={letter}>{letter}</button>
+        ))
+      }
+
     </div>
   );
 };
